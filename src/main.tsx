4234334666171ba4fa386d89f94f760a2597c9dc
mphomolefe730 <mphomolefe730@gmail.com>
2025-10-24 00:17:37 +0200

    add dashboard component

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,16 @@ import HomePage from './modules/homepage/components/homepage.tsx';
 import InteractiveMode from './modules/interactive_mode/components/interactiveMode.tsx';
 import EditMode from './modules/editor_mode/components/editorMode.tsx';
 import EditModeForm from './modules/editor_mode/components/editModeForm.tsx';
+import Dashboard from './modules/dashboard/components/dashboard.tsx';
 
 const router = createBrowserRouter([
   { 
     path: '/', 
     element: <HomePage/>,
     errorElement: <div><p>404 NOT FOUND</p></div>
+  },{
+    path: '/dashboard',
+    element: <Dashboard/>,
   },{
     path: '/interactive/:courseId/:courseSection',
     element: <InteractiveMode/>,
