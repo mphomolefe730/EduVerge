8b5740f98981eeed6cb7574a250c2836b85fb61a
koketso <koketsor97@gmail.com>
2025-11-02 13:49:34 +0200

    Adding and removing a few things

--- src/modules/homepage/components/homepage.tsx
+++ src/modules/homepage/components/homepage.tsx
@@ -23,7 +23,7 @@ function HomePage(){
 					<a href="#" className="nav-link text-gray-300 hover:text-white font-medium">Study Guides</a>
 					<a href="#" className="nav-link text-gray-300 hover:text-white font-medium">Analytics</a>
 					<a href="#" className="nav-link text-gray-300 hover:text-white font-medium">Contact</a>
-					<a href="login" className="bg-primary hover:bg-blue-600 text-white px-6 py-2 rounded-full font-medium transition duration-300">Login</a>
+					<a href="#" className="bg-primary hover:bg-blue-600 text-white px-6 py-2 rounded-full font-medium transition duration-300">Login</a>
 					</nav>
 				</div>
 				
